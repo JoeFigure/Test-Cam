7c2084d85f9df4995a1cbbe0fe03876a5e16a757
Joe Kletz <joekletz@joes-imac.local>
2015-10-18 14:09:34 +0100

    Initial Commit

---
+++ Test Cam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Cam
+//
+//  Created by Joe Kletz on 18/10/2015.
+//  Copyright © 2015 Joe Kletz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
